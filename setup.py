bbdf3321a98b42fc9a0c68a138aec305d3f9788a
Riley Flynn <riley@rileyflynn.me>
2017-02-25 15:17:12 -0330

    Began setting up setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="Jigsaw",
+    version="1.0",
+    packages=["jigsaw", ],
+    license="MIT",
+    description="A plugin framework for Python3.6+",
+    long_description="A plugin framework for Python3.6+"
+)
