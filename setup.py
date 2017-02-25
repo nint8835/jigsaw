48da60f182bd4c0482b12cd0e60b596838444312
Riley Flynn <riley@rileyflynn.me>
2017-02-25 15:37:51 -0330

    Fixed up setup.py more.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name="Jigsaw",
@@ -6,5 +6,18 @@ setup(
     packages=["jigsaw", ],
     license="MIT",
     description="A plugin framework for Python3.6+",
-    long_description="A plugin framework for Python3.6+"
+    long_description="A plugin framework for Python3.6+",
+    url="https://github.com/nint8835/jigsaw",
+    author="nint8835",
+    author_email="riley@rileyflynn.me",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Plugins",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Utilities"
+    ]
 )
