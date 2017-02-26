ec68d0c1c643dd9d3a431d9aa0bc330c0a411410
Riley Flynn <riley@rileyflynn.me>
2017-02-25 20:43:17 -0330

    Updated version number in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="Jigsaw",
-    version="1.1.0",
+    version="2.0.0",
     packages=["jigsaw", ],
     license="MIT",
     description="A plugin framework for Python3.6+",
