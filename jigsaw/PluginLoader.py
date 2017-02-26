3007b9feeb59ce73c07fe8ec5588c2a76de56a7f
Riley Flynn <riley@rileyflynn.me>
2017-02-25 21:31:10 -0330

    Added documentation

--- jigsaw/PluginLoader.py
+++ jigsaw/PluginLoader.py
@@ -11,7 +11,13 @@ from .Plugin import JigsawPlugin
 
 class PluginLoader(object):
 
-    def __init__(self, plugin_path: str="", log_level=logging.INFO, plugin_class = JigsawPlugin):
+    def __init__(self, plugin_path: str="", log_level=logging.INFO, plugin_class=JigsawPlugin) -> None:
+        """
+        Initializes the plugin loader
+        :param plugin_path: Path to load plugins from
+        :param log_level: Log level
+        :param plugin_class: Parent class of all plugins
+        """
         logging.basicConfig(format="{%(asctime)s} (%(name)s) [%(levelname)s]: %(message)s",
                             datefmt="%x, %X",
                             level=log_level)
@@ -30,7 +36,10 @@ class PluginLoader(object):
         self._plugins = {}
         self._modules = {}
 
-    def load_manifests(self):
+    def load_manifests(self) -> None:
+        """
+        Loads all plugin manifests on the plugin path
+        """
         for item in os.listdir(self.plugin_path):
             item_path = os.path.join(self.plugin_path, item)
             if os.path.isdir(item_path):
@@ -51,14 +60,29 @@ class PluginLoader(object):
                     self._logger.debug("No plugin manifest found at {}.".format(manifest_path))
 
     def get_manifest(self, plugin_name: str) -> dict:
+        """
+        Gets the manifest for a specified plugin
+        :param plugin_name: The name of the plugin
+        :return: The manifest for the specified plugin
+        """
         for manifest in self._manifests:
             if manifest["name"] == plugin_name:
                 return manifest
 
     def get_plugin_loaded(self, plugin_name: str) -> bool:
+        """
+        Returns if a given plugin is loaded
+        :param plugin_name: The plugin to check to loaded status for
+        :return: Whether the specified plugin is loaded
+        """
         return plugin_name in self._plugins
 
-    def load_plugin(self, manifest: dict, *args):
+    def load_plugin(self, manifest: dict, *args) -> None:
+        """
+        Loads a plugin from the given manifest
+        :param manifest: The manifest to use to load the plugin
+        :param args: Arguments to pass to the plugin
+        """
         if self.get_plugin_loaded(manifest["name"]):
             self._logger.debug("Plugin {} is already loaded.".format(manifest["name"]))
             return
@@ -104,17 +128,35 @@ class PluginLoader(object):
                 f.write(traceback.format_exc(5))
             self._logger.error("Failed to load plugin {}. Error log written to {}.".format(manifest["name"], exc_path))
 
-    def load_plugins(self, *args):
+    def load_plugins(self, *args) -> None:
+        """
+        Loads all plugins
+        :param args: Arguments to pass to the plugins
+        """
         for manifest in self._manifests:
             self.load_plugin(manifest, *args)
 
     def get_plugin(self, name: str) -> JigsawPlugin:
+        """
+        Gets a loaded plugin
+        :param name: Name of the plugin
+        :return: The plugin
+        """
         return self._plugins[name]
 
     def get_module(self, name: str):
+        """
+        Gets the module for a plugin
+        :param name: Name of the plugin
+        :return: The module
+        """
         return self._modules[name]
 
     def get_all_plugins(self) -> List[dict]:
+        """
+        Gets all loaded plugins
+        :return: List of all plugins
+        """
         return [{
             "manifest": i,
             "plugin": self.get_plugin(i["name"]),
