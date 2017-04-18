67165de3554c0caa401cc94a3fcb63727358d55a
Riley Flynn <riley@rileyflynn.me>
2017-04-18 18:25:49 -0230

    Fixed incorrectly named method in example program and also changed the way it worked to allow it to work on platforms other than windows

--- example/Example_Program.py
+++ example/Example_Program.py
@@ -4,7 +4,7 @@ from jigsaw import PluginLoader
 
 # Initialize the main jigsaw loader class
 loader = PluginLoader(
-    (os.path.absdir(os.path.join(__file__, "plugins")), )  # The folder(s) that plugins will be loaded from
+    (os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "plugins")), )  # The folder(s) that plugins will be loaded from
 )
 
 # Discover and load all the information required to load plugins
