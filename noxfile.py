219337f7797d634fa5bf5eb7dfcd5c60a0920c63
Riley Flynn <riley@rileyflynn.me>
2022-05-18 22:03:00 -0230

    Implement Nox (#14)

---
+++ noxfile.py
@@ -0,0 +1,10 @@
+try:
+    from nox_poetry import session
+except ImportError:
+    from nox import session
+
+
+@session(python=["3.7", "3.8", "3.9", "3.10"])
+def test(session):
+    session.install("pytest", ".")
+    session.run("pytest")
